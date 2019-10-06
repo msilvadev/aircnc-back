530301c94fe465333beb5ec12359e42dcc99181a
Matheus Silva <msilva.jus@gmail.com>
2019-10-05 22:56:57 -0300

    ajuste porta

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+
 const routes = require('./routes');
 
 const app = express();
@@ -21,4 +22,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
