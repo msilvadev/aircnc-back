5fcd936536cb227d203bc4b2ffbfb51ab927c666
Matheus Silva <msilva.jus@gmail.com>
2019-10-06 01:07:22 -0300

    Ajuste no caminho da imagem do spot para usar variavel de ambiente

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -17,7 +17,7 @@ const SpotSchema = new mongoose.Schema({
 
 SpotSchema.virtual('thumbnail_url').get(function() {
     //return `http://localhost:3333/files/${this.thumbnail}`
-    return `${process.env.APP_URL}/files/${this.thumbnail}`
+    return `https://aircnc-backend-node.herokuapp.com/files/${this.thumbnail}`
 })
 
 module.exports = mongoose.model('Spot', SpotSchema);
